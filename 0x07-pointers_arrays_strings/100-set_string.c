e544341596f36875d50deff5077579aea6b5531d
Jerrylyte <ojonuba93@gmail.com>
2022-03-28 09:08:22 -0700

    set strings

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,13 +1,25 @@
-#include "holberton.h"
+#include "main.h"
+
+
 
 /**
- * set_string - set the value of a pointer to a character.
- * @s: pointer to a pointer to a char
- * @to: the string s should point to.
- *
- * Return: nothing.
- */
+
+* set_string - set the value of a pointer to a character.
+
+* @s: pointer to a pointer to a char
+
+* @to: the string s should point to.
+
+*
+
+* Return: nothing.
+
+*/
+
 void set_string(char **s, char *to)
+
 {
-	*s = to;
+
+*s = to;
+
 }
